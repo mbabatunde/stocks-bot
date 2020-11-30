f8dd0109858e902520f4a70be206488472393256
Mark Babatunde <markababatunde@gmail.com>
2020-11-29 19:29:25 -0500

    changing bot status

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const finnhubClient = new finnhub.DefaultApi();
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('ready!');
+	client.user.setActivity('PLTR 🚀🌕', { type: 'WATCHING' });
 });
 
 
